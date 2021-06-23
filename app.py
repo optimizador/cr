132fac21adbcd77e201c615d29314d5d80d250f8
benjicc <benjicc1807972@gmail.com>
2021-06-23 17:25:56 -0500

    dimensionamiento Container registry

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/dimensionamiento/cloud_pak')
+def dim_cp():
+    return render_template('dim_cp.html')
